7c2f25fcf277efaa5de771eb54f9b09f6eceb4e7
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2020-09-25 20:51:35 +0100

    Add imports for app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
 import './App.css'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 const App: React.FunctionComponent = () => {
   return <div className='App'>Chat Room</div>
