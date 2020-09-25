455abdea5df52cc462d5735dda640074a85315c2
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2020-09-25 20:55:04 +0100

    Initialize auth and firestore

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,6 +6,13 @@ import './App.css'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 
+firebase.initializeApp({
+  // GitHub don't track my config!
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 const App: React.FunctionComponent = () => {
   return <div className='App'>Chat Room</div>
 }
